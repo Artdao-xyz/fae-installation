13487986a1fc027bcbc60c29b24dfdade0a4f20d
pedrofalco <fpfalco@gmail.com>
2026-04-30 10:19:04 -0300

    add one more pixel to the popup

--- src/app/HomeTopPopUpStrip.tsx
+++ src/app/HomeTopPopUpStrip.tsx
@@ -8,7 +8,7 @@ export function HomeTopPopUpStrip() {
   const leftStyle = `calc(var(--width-filter-chrome-column) + ${EXTRA_LEFT_OFFSET_PX}px)`;
 
   return (
-    <div className="pointer-events-none fixed inset-x-0 top-0 z-30 hidden h-(--inset-margin-guide) bg-[#E9E9E9] lg:block">
+    <div className="pointer-events-none fixed inset-x-0 top-0 z-30 hidden h-[calc(var(--inset-margin-guide)+1px)] bg-[#E9E9E9] lg:block">
       <div className="relative h-full w-full">
         <div
           className="pointer-events-auto absolute top-1/2 -translate-y-1/2"
